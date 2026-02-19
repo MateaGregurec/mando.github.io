1288a9be4bb0af1f1a963f36aa867a1dadf82366
MateaGregurec <gregurecmatea@gmail.com>
2026-02-19 22:25:17 +0100

    dost dobro3

--- main.js
+++ main.js
@@ -3,10 +3,13 @@
 
 const menu = document.querySelector('#mobile-menu'); //sve sto ima mobile menu id sad je nazvano menu
 const menu_links = document.querySelector('.navbar_menu');
-menu.addEventListener('click',function(){
+menu.addEventListener('click', toggleMenu);
+menu.addEventListener('touchstart', toggleMenu);
+
+function toggleMenu(){
     menu.classList.toggle('is-active');     //click, onda prebaci klasu na is-active; ako vec postoji, onda ju makne  
     menu_links.classList.toggle('active');  //click, prebaci klasu na active; ako ju ima, makne ju 
-});
+}
 
 
 const observer = new IntersectionObserver(entries => {
