99fe42a8f6ad0b1f7a9cc56ba2842ad24492e49f
MateaGregurec <gregurecmatea@gmail.com>
2026-02-19 19:27:49 +0100

    responsive fully2

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
+      observer.unobserve(entry.target);
     }
   });
 });
